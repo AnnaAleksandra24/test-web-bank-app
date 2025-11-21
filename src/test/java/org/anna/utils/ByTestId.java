9cd2bf8c0a5462f84ec04bd1acb8743be84ab39c
Anna Aleksandra <aleksandraanna24@gmail.com>
2025-11-21 23:08:16 +0000

    feat(01): Improve HomePage tests

---
+++ src/test/java/org/anna/utils/ByTestId.java
@@ -0,0 +1,10 @@
+package org.anna.utils;
+
+import org.openqa.selenium.By;
+
+public class ByTestId {
+    public static By testId (String testid){
+        return By.cssSelector("[data-testid='" + testid + "']");
+
+    }
+}
